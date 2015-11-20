6bbf4f7ef97f2bdbbf63bb07d9546d4011336268
dmytsang <dmytsang@gmail.com>
2015-11-21 10:33:17 +1300

    changing to portrait

--- AppWIP/js/main.js
+++ AppWIP/js/main.js
@@ -1,23 +1,46 @@
 //this game will have only 1 state
 var GameState = {
+  
+  init: function() {
+    //adapt to screen size, fit all the game
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+
+  },
+  
   //load the game assets before the game starts
   preload: function() {
     this.load.image('background', 'assets/images/background.png');
+    this.load.image('toneOneButton', 'assets/images/btnTone1.png');   
+    this.load.image('toneTwoButton', 'assets/images/btnTone2.png');   
+    this.load.image('toneThreeButton', 'assets/images/btnTone3.png');   
+    this.load.image('toneFourButton', 'assets/images/btnTone4.png');   
     
   },
   //executed after everything is loaded
   create: function() {
     this.background = this.game.add.sprite(0, 0, 'background');
+    
+    this.createOnscreenControls();
   },
   //this is executed multiple times per second
   update: function() {
   },
+ createOnscreenControls: function(){
+    this.toneOne = this.add.button(20, 300, 'toneOneButton');
+    this.toneTwo = this.add.button(50, 300, 'toneTwoButton');
+    this.toneThree = this.add.button(80, 300, 'toneThreeButton');
+    this.toneFour = this.add.button(110, 300, 'toneFourButton');
+    
+  }
   
-
 };
 
 //initiate the Phaser framework
-var game = new Phaser.Game(640, 360, Phaser.AUTO);
+var game = new Phaser.Game(360, 640, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
